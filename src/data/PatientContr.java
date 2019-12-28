47c9a48d45e1bf8c4f590b9b719330407d71d54a
pau1838 <pauu1838@gmail.com>
2019-12-28 13:28:14 +0100

    Implementada classe PatientContr

--- src/data/PatientContr.java
+++ src/data/PatientContr.java
@@ -1,4 +1,48 @@
 package data;
 
-public class PatientContr {
+import data.exceptions.FormatErrorException;
+
+import java.math.BigDecimal;
+
+final public class PatientContr {
+    private final BigDecimal contribution;
+
+    public PatientContr(BigDecimal contribution) throws NullPointerException, FormatErrorException {
+        if (contribution==null) {
+            throw new NullPointerException("Rebut objecte sense instanciar.");
+        }
+
+        if (isValidContr(contribution)) {
+            this.contribution = contribution;
+        } else {
+            throw new FormatErrorException("Error amb el format de l'aportació del pacient.");
+        }
+    }
+
+    public BigDecimal getContribution() { return contribution; }
+
+    @Override
+    public int hashCode() { return contribution.hashCode(); }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PatientContr patientContr = (PatientContr) o;
+        return (contribution.compareTo(patientContr.contribution)==0);
+    }
+
+    @Override
+    public String toString() {
+        return "PatientContribution(" + "contribution='" + contribution + '\'' + '}';
+    }
+
+    private boolean isValidContr(BigDecimal contribution) {
+        if (contribution.compareTo(new BigDecimal("0.0")) == -1 ||
+            contribution.compareTo(new BigDecimal("100.0")) == 1) {
+            return false;
+        }
+
+        return true;
+    }
 }
