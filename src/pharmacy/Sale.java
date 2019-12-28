6e8262adeadb18c8e32ad19f38d8bbc3f079b197
pau1838 <pauu1838@gmail.com>
2019-12-29 00:13:31 +0100

    Creades classes Dispensing i Sale

---
+++ src/pharmacy/Sale.java
@@ -0,0 +1,53 @@
+package pharmacy;
+
+import data.PatientContr;
+import data.ProductID;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+/***
+ * Package for the classes involved in the use case Suply next dispensing
+ */
+
+public class Sale {
+    private int saleCode;
+    private Date date;
+    private BigDecimal amount;
+    private boolean isClosed; // flag to know if the sale is closed
+    // ??? Its components, among others
+
+    public Sale() {
+
+    }
+
+    public void addLine(ProductID prodID, BigDecimal price, PatientContr contr) throws SaleClosedException {
+
+    }
+
+    private void calculateAmount() {
+
+    }
+
+    private void addTaxes() {
+
+    }
+
+    public void calculateFinalAmount() throws SaleClosedException {
+
+    }
+
+    public BigDecimal getAmount() {
+
+    }
+
+    private void setClosed() {
+
+    }
+
+    public boolean isClosed() {
+
+    }
+
+    // the rest of getters, setters and methods
+}
