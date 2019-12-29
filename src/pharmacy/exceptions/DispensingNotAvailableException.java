73ebcf94f4e583ee5c4628e987b2f7210f1fa28e
pau1838 <pauu1838@gmail.com>
2019-12-29 16:51:26 +0100

    Implementada classe Dispensing Creada classe MedicineDispensingLine Implementada excepció DispensingNotAvailableException

---
+++ src/pharmacy/exceptions/DispensingNotAvailableException.java
@@ -0,0 +1,7 @@
+package pharmacy.exceptions;
+
+public class DispensingNotAvailableException extends Throwable {
+    public DispensingNotAvailableException(String message) {
+        super(message);
+    }
+}
