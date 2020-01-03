9bb80cca2056af487e2eddfd2f88aa7de58aeb03
pau1838 <pauu1838@gmail.com>
2020-01-03 17:20:33 +0100

    Implementades associacions entre ProductSaleLine-MedicineDispensingLine-ProductSpecification (faltaven associacions de ProductSpecification)

--- src/pharmacy/ProductSpecification.java
+++ src/pharmacy/ProductSpecification.java
@@ -8,6 +8,14 @@ public class ProductSpecification {
     private ProductID prodID;
     private String description;
     private BigDecimal price;
+    private MedicineDispensingLine medDispensingLine;
+    private ProductSaleLine prodSaleLine;
+
+    public ProductSpecification(ProductID productID, String description, BigDecimal price) {
+        this.prodID = productID;
+        this.description = description;
+        this.price = price;
+    }
 
     public BigDecimal getPrice() {
         return price;
@@ -15,5 +23,13 @@ public class ProductSpecification {
 
     public String getDescription() {
         return description;
+    }
+
+    public void setMedDispensingLine(MedicineDispensingLine medDispensingLine) {
+        this.medDispensingLine = medDispensingLine;
+    }
+
+    public void setProdSaleLine(ProductSaleLine prodSaleLine) {
+        this.prodSaleLine = prodSaleLine;
     }
 }
