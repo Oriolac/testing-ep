e06c663e38198a81f2bfc395a60336c1ae073f8e
pau1838 <pauu1838@gmail.com>
2020-01-03 17:57:14 +0100

    Creada excepció PatientIDException + completades throws de capçaleres de mètodes

---
+++ src/pharmacy/exceptions/PatientIDException.java
@@ -0,0 +1,7 @@
+package pharmacy.exceptions;
+
+public class PatientIDException extends Exception {
+    public PatientIDException(String message) {
+        super(message);
+    }
+}
--- src/services/HealthCardReader.java
+++ src/services/HealthCardReader.java
@@ -1,6 +1,7 @@
 package services;
 
 import data.HealthCardID;
+import data.exceptions.HealthCardException;
 
 public interface HealthCardReader {
     HealthCardID getHealthCardID() throws HealthCardException;
