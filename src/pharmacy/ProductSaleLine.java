111c43e6edbc0e4a9a08ab367bd1bfd8b54e14c6
pau1838 <pauu1838@gmail.com>
2020-01-03 12:25:23 +0100

    MedicineDispensingLine, ProductSaleLine -> Continuada implementació

--- src/pharmacy/ProductSaleLine.java
+++ src/pharmacy/ProductSaleLine.java
@@ -6,6 +6,8 @@ import data.ProductID;
 import java.math.BigDecimal;
 
 public class ProductSaleLine {
+    private Sale sale;
+    private MedicineDispensingLine medDispensingLine;
     private ProductSpecification productSpec;
     private BigDecimal subtotal;
 
