4c66e33456e9afb455ea582660d73d0f772ef847
pau1838 <pauu1838@gmail.com>
2020-01-03 12:25:23 +0100

    MedicineDispensingLine, ProductSaleLine -> Continuada implementació

--- src/pharmacy/MedicineDispensingLine.java
+++ src/pharmacy/MedicineDispensingLine.java
@@ -1,13 +1,32 @@
 package pharmacy;
 
+
 public class MedicineDispensingLine {
     private boolean acquired;
+    private ProductSpecification productSpec;
+    private ProductSaleLine productSaleLine;
+    private Dispensing ePrescription;
 
-    public MedicineDispensingLine() {
+    public MedicineDispensingLine(Dispensing ePrescription, ProductSpecification productSpec) {
+        this.ePrescription = ePrescription;
+        acquired = false;
+        this.productSpec = productSpec;
+        productSaleLine = null;
+    }
 
+    public boolean isAcquired() {
+        return acquired;
+    }
+
+    public ProductSpecification getProductSpec() {
+        return productSpec;
     }
 
     public void setAcquired() {
         acquired = true;
+    }
+
+    public void setProductSaleLine(ProductSaleLine productSaleLine) {
+        this.productSaleLine = productSaleLine;
     }
 }
