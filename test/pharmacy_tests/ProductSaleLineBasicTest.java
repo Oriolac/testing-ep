128eba37b5d904d60761b504f3d87c36d1937fe8
pau1838 <pauu1838@gmail.com>
2020-01-04 01:29:56 +0100

    Millorat ProductSpecificationTest, Implementat ProductSaleLineBasicTest

---
+++ test/pharmacy_tests/ProductSaleLineBasicTest.java
@@ -0,0 +1,38 @@
+package pharmacy_tests;
+
+import data.PatientContr;
+import data.exceptions.FormatErrorException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pharmacy.MedicineDispensingLine;
+import pharmacy.ProductSaleLine;
+import pharmacy.ProductSpecification;
+import pharmacy.Sale;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ProductSaleLineBasicTest {
+    private ProductSaleLine productSaleLine;
+
+    @BeforeEach
+    public void initProductSaleLine() throws FormatErrorException {
+        Sale sale = new Sale();
+        ProductSpecification prodSpec = new ProductSpecification();
+        MedicineDispensingLine medDispensingLine = new MedicineDispensingLine(prodSpec);
+        BigDecimal price = new BigDecimal("10.0");
+        PatientContr patientContr = new PatientContr(new BigDecimal("0.5"));
+        productSaleLine = new ProductSaleLine(sale, medDispensingLine, price, patientContr);
+    }
+
+    @Test
+    public void getSubtotalTest() {
+        BigDecimal exp_subtotal = new BigDecimal("5.0");
+        BigDecimal obt_subtotal = productSaleLine.getSubtotal();
+        BigDecimal n_exp_subtotal = new BigDecimal("4.0");
+
+        assertTrue(exp_subtotal.compareTo(obt_subtotal) == 0);
+        assertTrue(n_exp_subtotal.compareTo(obt_subtotal) < 0);
+    }
+}
