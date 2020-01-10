70a8d368d00b8f3705fda8aab13fcd4a6d2fab84
pau1838 <pauu1838@gmail.com>
2020-01-10 12:14:27 +0100

    SaleTest -> Inicialitats medicaments dispensables

--- test/pharmacy_tests/SaleTest.java
+++ test/pharmacy_tests/SaleTest.java
@@ -1,8 +1,12 @@
 package pharmacy_tests;
 
+import data.DispensableMedicines;
+import data.ProductID;
+import data.exceptions.ProductIDException;
 import org.junit.jupiter.api.BeforeEach;
 import pharmacy.Dispensing;
 import pharmacy.DispensingTerminal;
+import pharmacy.MedicineDispensingLine;
 import pharmacy.Sale;
 
 public class SaleTest {
@@ -10,11 +14,20 @@ public class SaleTest {
     private Sale sale;
 
     @BeforeEach
-    public void initSale() {
-        Dispensing ePrescription = new Dispensing();
+    public void initSale() throws ProductIDException {
         DispensingTerminal dispensingTerminal = new DispensingTerminal();
+        DispensableMedicines dispensableMedicines = initDispensableMedicines(dispensingTerminal);
+        Dispensing ePrescription = new Dispensing(dispensableMedicines);
         sale = new Sale(dispensingTerminal, ePrescription);
     }
 
 
+
+    private DispensableMedicines initDispensableMedicines(DispensingTerminal dt) throws ProductIDException {
+        DispensableMedicines dispensableMedicines = new DispensableMedicines();
+        dispensableMedicines.put(new ProductID("111111111111"), new MedicineDispensingLine());
+        dispensableMedicines.put(new ProductID("222222222222"), new MedicineDispensingLine());
+        dispensableMedicines.put(new ProductID("333333333333"), new MedicineDispensingLine());
+        return dispensableMedicines;
+    }
 }
