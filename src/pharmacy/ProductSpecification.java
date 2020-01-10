f46e43df9a9e19bd0a19bd2efebe4e656902d3d0
pau1838 <pauu1838@gmail.com>
2020-01-10 12:48:25 +0100

    MedicineDispensingLine, ProductSaleLine, ProductSpecification -> Implementats equals

--- src/pharmacy/ProductSpecification.java
+++ src/pharmacy/ProductSpecification.java
@@ -17,6 +17,11 @@ public class ProductSpecification {
         this.price = price;
     }
 
+    public boolean equals(ProductSpecification productSpec) {
+        return prodID.equals(productSpec.getProdID()) && description.equals(description)
+                && price.equals(productSpec.getPrice());
+    }
+
     public ProductID getProdID() {
         return prodID;
     }
