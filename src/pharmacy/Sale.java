c061b05a61ab72377e85557437b7085c7e7177ce
pau1838 <pauu1838@gmail.com>
2020-01-10 11:29:29 +0100

    Code Smell "Primitive Obsession" -> Refactoring "Replace Data Value with Object" en Dispensing

--- src/pharmacy/Sale.java
+++ src/pharmacy/Sale.java
@@ -66,7 +66,7 @@ public class Sale {
     }
 
     private boolean isDispensable(ProductID prodId) {
-        return ePrescription.getMedicineDispensingLines().containsKey(prodId);
+        return ePrescription.getDispensableMedicines().contains(prodId);
     }
 
     public void calculateFinalAmount() throws SaleClosedException {
